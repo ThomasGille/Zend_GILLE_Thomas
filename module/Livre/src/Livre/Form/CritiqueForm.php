55f68c5245bc9043a019e4d2a768801e8f794d19
ThomasGille <totogille@gmail.com>
2016-01-20 19:36:16 +0100

    tests

--- module/Livre/src/Livre/Form/CritiqueForm.php
+++ module/Livre/src/Livre/Form/CritiqueForm.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,27 +8,37 @@
 
 namespace Livre\Form;
 
- use Zend\Form\Form;
+use Zend\Form\Form;
 
- class CritiqueForm extends Form
- {
-     public function __construct($name = null)
-     {
-         // we want to ignore the name passed
-         parent::__construct('livre');
+class CritiqueForm extends Form {
 
-         $this->add(array(
-             'name' => 'idUser',
-             'type' => 'Hidden',
-         ));
-         $this->add(array(
-             'name' => 'idLivre',
-             'type' => 'Hidden',
-         ));
-         $this->add(array(
-             'name' => 'Note',
-             'type' => 'Integer',
-         ));
+    public function __construct($name = null) {
+        // we want to ignore the name passed
+        parent::__construct('critique');
 
-     }
- }
+        $this->add(array(
+            'name' => 'idUser',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'idLivre',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'Note',
+            'type' => 'Hidden',
+            'options' => array(
+                'label' => 'Auteur',
+            ),
+        ));
+        $this->add(array(
+             'name' => 'submit',
+             'type' => 'Submit',
+             'attributes' => array(
+                 'value' => 'Go',
+                 'id' => 'submitbutton',
+             ),
+         ));
+    }
+
+}
