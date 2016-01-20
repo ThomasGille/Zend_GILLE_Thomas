e78faa8b649bcab7e7676f377d06f127e965e1a5
ThomasGille <totogille@gmail.com>
2016-01-20 18:52:07 +0100

    tests

--- BD.sql
+++ BD.sql
@@ -1,3 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 20 Janvier 2016 à 18:51
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `zf2tutorial`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `album`
+--
 
 CREATE TABLE IF NOT EXISTS `album` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -7,12 +34,56 @@ CREATE TABLE IF NOT EXISTS `album` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
+--
+-- Contenu de la table `album`
+--
+
 INSERT INTO `album` (`id`, `idUser`, `artist`, `title`) VALUES
 (1, 2, 'The  Military  Wives', 'In  My  Dreams'),
 (2, 1, 'Adele', '21'),
 (3, 2, 'Bruce  Springsteen', 'Wrecking Ball (Deluxe)'),
 (4, 1, 'Lana  Del  Rey', 'Born  To  Die'),
 (5, 1, 'Gotye', 'Making  Mirrors');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `critique`
+--
+
+CREATE TABLE IF NOT EXISTS `critique` (
+  `idUser` int(11) NOT NULL,
+  `idLivre` int(11) NOT NULL,
+  `Note` int(11) NOT NULL,
+  PRIMARY KEY (`idUser`,`idLivre`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `livre`
+--
+
+CREATE TABLE IF NOT EXISTS `livre` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) NOT NULL,
+  `auteur` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `livre`
+--
+
+INSERT INTO `livre` (`id`, `title`, `auteur`) VALUES
+(1, 'La patate, ma vie', 'Moi'),
+(2, 'La carotte', 'Toi');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
 
 CREATE TABLE IF NOT EXISTS `users` (
   `idUser` int(11) NOT NULL AUTO_INCREMENT,
@@ -21,27 +92,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`idUser`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Contenu de la table `users`
+--
+
 INSERT INTO `users` (`idUser`, `user_name`, `pass_word`) VALUES
 (1, 'root', 'root');
 
-CREATE TABLE IF NOT EXISTS `livre` (
-  `id` int(11) NOT NULL,
-  `title` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-ALTER TABLE `livre`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `livre`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
-
-CREATE TABLE IF NOT EXISTS `critique` (
-  `idUser` int(11) NOT NULL,
-  `idLivre` int(11) NOT NULL,
-  `note` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
-ALTER TABLE `critique`
-  ADD PRIMARY KEY (`idUser`,`idLivre`);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
